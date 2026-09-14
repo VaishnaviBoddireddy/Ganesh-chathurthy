01b1f9754b661c92462337eb7d045bea87b591fe
VAISHNAVI BODDDIREDDY <boddireddy100052000@gmail.com>
2026-09-14 13:59:33 +0530

    Fix selector by removing 'ganesha' class

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const replayButton = document.getElementById("replayButton");
 replayButton.addEventListener("click", function () {
 
     const animatedElements = document.querySelectorAll(
-        ".brand, .ganesha, .light-rays, .light, .flower-decoration"
+        ".brand,  .light-rays, .light, .flower-decoration"
     );
 
     animatedElements.forEach(function (element) {
